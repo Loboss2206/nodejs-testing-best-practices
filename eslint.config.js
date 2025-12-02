93395c37f7678919514240c67e79dea6d4ae1cee
Logan Brunet <98391380+Loboss2206@users.noreply.github.com>
2025-12-02 10:34:08 +0100

    change nodejs.yml

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommended: true,
+});
+
+export default [
+    ...compat.config({
+        extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    }),
+    {
+        files: ["*.ts", "*.tsx"],
+        parser: "@typescript-eslint/parser",
+        parserOptions: {
+            ecmaVersion: 2020,
+            sourceType: "module",
+        },
+        rules: {
+            // tes règles personnalisées ici
+        },
+    },
+];
